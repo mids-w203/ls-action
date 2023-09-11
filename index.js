0aa6428354f5f3a835d6953d3c260118c248c67c
Kmart <blulightspecial@users.noreply.github.com>
2023-09-11 10:01:53 -0700

    Added comments to index.js. It still needs updates for fall.

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 // Build: npm run build
 //
-const year = '23';
-const semester = 'fall'; // long name
-const sem = 'fa' // short name
+const year = '23';       // 2 digit year as string (e.g. '23' for 2023)
+const semester = 'fall'; // long name ('fall','spring', or 'summer')
+const sem = 'fa'         // short name ('fa','sp', or 'su') 
 const sections = {
-    2: [1,2,3,4],
-    3: [5,6],
-    4: [7,8,9]
+// day: [section numbers]
+// day - Monday is 1 thru Sunday is 7
+    2: [1,2,3,4],        // Tuesday sections
+    3: [5,6],            // Wednesday sections
+    4: [7,8,9]           // Thursday sections
 };
 const mids_weeks = [
+    // Iso weeks where we have class
+    // first entry will be week 1 for hw/ls, second will be week 2, etc.
     19,20,21,22,23,24,25,26,27,28,29,30,31,32
 ];
 const hw_sol_release_weeks = [
+    // Weeks where we have homeworks released to students
     2,3,5
-]; // this is mids week/unit
+]; 
 const hw_release_day = 4 // Monday = 1, etc.
 
 process.env.TZ = 'America/Los_Angeles' ;
